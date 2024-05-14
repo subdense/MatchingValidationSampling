86ddc5e0b9456c55d6a7fe58db3dd8838ab848bd
Juste Raimbault <juste.raimbault@ign.fr>
2024-05-14 17:11:58 +0200

    grid_isochr

--- sampling.R
+++ sampling.R
@@ -8,7 +8,6 @@ setwd(paste0(Sys.getenv('CS_HOME'),'/SuburbanDensification/Models/Matching/Valid
 #source('gdalUtils.R') # code version: https://github.com/gearslaboratory/gdalUtils/tree/784b61c43727cbc9d27b275aa78d3e707ef04225
 
 library(sf)
-library(r5r) # install rJava with apt : https://github.com/s-u/rJava/issues/255 (fails jni)
 library(osmdata)
 library(osmextract)
 library(rjson)
@@ -19,17 +18,19 @@ library(httr)
 #library(usethis)
 
 # cities and their countries (needed for OSM data download)
-#cities = list('Strasbourg'=c('France','Germany'),
-#              'Toulouse'=c('France'),
+cities = list(
+#  'Strasbourg'=c('France','Germany'),
+              'Toulouse'=c('France')#,
 #              'Dortmund'=c('Germany'),
 #              'Frankfurt'=c('Germany'),
 #              'Liverpool'=c('England', 'Wales'),
 #              'Bristol'=c('England', 'Wales')
-#              )
-cities=list('Luxembourg'=c('Luxembourg')) # test
+              )
+
+#cities=list('Luxembourg'=c('Luxembourg')) # test
 # radius in which networks are constructed to build isochrones
-#max_bbox_radius = 400 #km 
-max_bbox_radius = 1
+max_bbox_radius = 100 #km 
+#max_bbox_radius = 1
 
 osm_data_dir = './data/osm/'
 data_dir = './data/'
@@ -84,6 +85,10 @@ for(city in names(cities)){
   #    st_intersection(allnw,broad_bbox)
   #  )
   #}
+  
+  #if(!cached_city_data){dir}
+  dir.create(paste0(data_dir,city),showWarnings = F)
+  
   for(country in cities[city]){
     datafile = paste0('geofabrik_',strsplit(tail(strsplit(oe_match(country)$url,'/',fixed=T)[[1]], n=1),'.',fixed=T)[[1]][1])
     
@@ -94,14 +99,14 @@ for(city in names(cities)){
     # ogr2ogr test.gpkg test.osm.pbf 
     #system(paste0('ogr2ogr ',osm_data_dir, datafile,'_',city,'_highways.gpkg ',osm_data_dir, datafile,'_',city,'_highways.osm.pbf'))
     
+    system(paste0('cp ',osm_data_dir, datafile,'_',city,'_highways.osm.pbf ',data_dir,city,'/highways.osm.pbf'))
+    
   }
   
-  dir.create(paste0(data_dir,city),showWarnings = F)
   
   # TODO merge if more than one country
   # cp data/osm/geofabrik_luxembourg-latest_Luxembourg_highways.osm.pbf 
-  # system(paste0('cp ',data_dir, city,'highways.osm.pbf'))
-  
+   
   #st_write(network, dsn = paste0(data_dir,city,'/network.gpkg'), layer = city)
   #sf::gdal_utils(util = "vectortranslate", source = paste0(data_dir,city,'/network.gpkg'), destination = paste0(data_dir,city,'/network.osm.pbf'),options = c("-f", "OSM"))
   
@@ -156,9 +161,13 @@ token = content(req)$access_token
 catalog = read_csv('https://storage.googleapis.com/storage/v1/b/mdb-csv/o/sources.csv?alt=media')
 
 
-# mdb ids
-gtfs_feeds = list('Luxembourg'=c(1108))
+# mdb ids in the catalog
+gtfs_feeds = list(
+  'Luxembourg'=c(1108),
+  'Toulouse'=c(1024)
+)
 
+# TODO handle multiple GTFS
 for(city in names(cities)){
   zipfile=paste0(data_dir,city,'/',city,'.zip')
   download.file(catalog$urls.latest[gtfs_feeds[[city]][1]],destfile = zipfile)
@@ -166,15 +175,70 @@ for(city in names(cities)){
 }
 
 
+Sys.setenv(JAVA_HOME = '/usr/lib/jvm/java-1.21.0-openjdk-amd64')
+
+
+library(r5r) # install rJava with apt : https://github.com/s-u/rJava/issues/255 (fails jni)
+
+
 # for java R5
-options(java.parameters = '-Xmx16G')
+options(java.parameters = '-Xmx32G')
 
-
-
+#Sys.setenv(JAVA_HOME = '/usr/lib/jvm/java-1.21.0-openjdk-amd64')
+# sudo R CMD javareconf JAVA_HOME=/usr/lib/jvm/java-1.21.0-openjdk-amd64
+# see https://stackoverflow.com/questions/28133360/rjava-is-not-picking-up-the-correct-java-version
 
 network <- setup_r5(data_path = paste0(data_dir,city))
 
+street_net <- street_network_to_sf(network)
 
+#orig = data.frame(id='1',st_coordinates(st_centroid(broad_bbox)))
+#names(orig)<-c('id','lon','lat')
+#dest = data.frame(id=as.character(1:4),st_coordinates(broad_bbox)[1:4,c(1,2)])
+#names(dest)<-c('id','lon','lat')
+
+#set.seed(42)
+#orig = data.frame(id='1',st_coordinates(street_net$vertices[sample.int(nrow(street_net$vertices), size=1),]));names(orig)<-c('id','lon','lat')
+#dest = data.frame(id='1',st_coordinates(street_net$vertices[sample.int(nrow(street_net$vertices), size=1),]));names(dest)<-c('id','lon','lat')
+#gc()
+
+#'
+#' constructs a point grid around the origin point (wgs84), with a given step (meters), of width/height 2*size+1
+point_grid <- function(orig, step, size){
+  p0 = st_coordinates(st_transform(orig,etrs89lcc))
+  x0 = p0[1,1]; y0 = p0[1,2]
+  xcoords = seq(from = x0 - size*step, to = x0 + size*step, by = step)
+  ycoords = seq(from = y0 - size*step, to = y0 + size*step, by = step)
+  allx = matrix(data=rep(xcoords,length(ycoords)),nrow = length(ycoords), byrow = T)
+  ally = matrix(data=rep(ycoords,length(xcoords)),ncol = length(xcoords), byrow = F)
+  return(st_transform(st_sfc(st_multipoint(as.matrix(data.frame(X = c(allx), Y=c(ally)))), crs = etrs89lcc), wgs84))
+}
+
+orig = data.frame(id='1',st_coordinates(st_centroid(broad_bbox))); names(orig)<-c('id','lon','lat')
+points_dest = point_grid(st_centroid(broad_bbox), 1000, 10)
+dest = st_coordinates(points_dest)
+dest = data.frame(id = as.character(1:nrow(dest)), lon=dest[,1], lat=dest[,2])
+
+# pete en mem avec 100km
+#isochrone = r5r::isochrone(network,origins = orig ,mode=c( "TRANSIT", "CAR"))
+#plot(isochrone[2,])
+travel_time = travel_time_matrix(network, orig, dest,mode=c("TRANSIT","CAR"),
+                                 mode_egress = "WALK",
+                                 max_trip_duration = 240,verbose = T, progress = T)
+dest$time = rep(NA, nrow(dest))
+dest$time[as.numeric(travel_time$to_id)] = travel_time$travel_time_p50
+ggplot(dest)+geom_point(aes(x=lon,y=lat,fill=time),size=2, shape=1)
+
+det = detailed_itineraries(network, orig, dest,mode=c("TRANSIT","CAR"),
+                           max_trip_duration = 240,verbose = T, progress = T)
+
+library(ggplot2)
+ggplot() +geom_sf(data = street_net$edges, color='gray85') +
+  geom_sf(data = det, aes(color=mode)) +
+  facet_wrap(.~option) + 
+  theme_void()
+
+# TODO heuristic to calculate approximate isochrones : PT+WALK+BICYCLE, CAR, PT+CAR
 
 
 # 2) Get buildings within the area
