4c3c04dca6aef4b97b5f86c6e099adc9a43d050c
Juste Raimbault <juste.raimbault@ign.fr>
2025-01-27 15:27:47 +0100

    clean network code

--- sampling.R
+++ sampling.R
@@ -1,11 +1,5 @@
 
-
 setwd(paste0(Sys.getenv('CS_HOME'),'/SuburbanDensification/Models/Matching/Validation/MatchingValidationSampling/'))
-
-#library(devtools)
-#devtools::install_github("gearslaboratory/gdalUtils")
-# -> fail, requires rgdal which has been discontinued
-#source('gdalUtils.R') # code version: https://github.com/gearslaboratory/gdalUtils/tree/784b61c43727cbc9d27b275aa78d3e707ef04225
 
 library(sf)
 library(osmdata)
@@ -14,101 +8,96 @@ library(rjson)
 library(dplyr)
 library(readr)
 library(httr)
-#library(gdalUtils)
-#library(usethis)
+library(ggplot2)
+
 
 # cities and their countries (needed for OSM data download)
-cities = list(
+#cities = list(
 #  'Strasbourg'=c('France','Germany'),
-              'Toulouse'=c('France')#,
+#              'Toulouse'=c('France')#,
 #              'Dortmund'=c('Germany'),
 #              'Frankfurt'=c('Germany'),
 #              'Liverpool'=c('England', 'Wales'),
 #              'Bristol'=c('England', 'Wales')
-              )
+#              )
+cities=list('Luxembourg'=c('Luxembourg', 'France')) # test
 
-#cities=list('Luxembourg'=c('Luxembourg')) # test
 # radius in which networks are constructed to build isochrones
-max_bbox_radius = 100 #km 
-#max_bbox_radius = 1
+#max_bbox_radius = 100 #km 
+max_bbox_radius = 30
+
+processing_steps = c(download_osm=T, road_network=T)
 
 osm_data_dir = './data/osm/'
 data_dir = './data/'
-
-dir.create(osm_data_dir,recursive = T, showWarnings = F)
-
-# 1) Get OSM data
-
-# setup download directory -> OSMEXT_DOWNLOAD_DIRECTORY environment variable
-#usethis::edit_r_environ()
-#readRenviron(".Renviron")
-Sys.setenv(OSMEXT_DOWNLOAD_DIRECTORY = osm_data_dir)
-
-#test <- oe_get("Strasbourg",quiet = FALSE,
-#               query = "SELECT * FROM 'lines' WHERE highway IS NOT NULL" # highway column: 78312 NAs out of 371577 with no filter
-#)
-
-# specifying regions above could save some data, or using city name matching in oe_get function, but this does not tackle the issue of trans-boundary regions
-options(timeout = 3600)
-for(country in unique(unlist(cities))){
-  #oe_get(country, download_only=T, skip_vectortranslate = T, max_file_size = 5e10)
-  oe_get(country, max_file_size = 5e10)
-}
-
-# uncomment for prod
-#write(rjson::toJSON(list(provider='geofabrik',date=as.character(Sys.Date()), path='./data/osm/')),'osm.json')
-
-
-# 2) Construct multimodal isochrone
 
 # https://www.crs-geo.eu/crs-pan-european.htm
 etrs89lcc = "+proj=lcc +lat_0=52 +lon_0=10 +lat_1=35 +lat_2=65 +x_0=4000000 +y_0=2800000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
 wgs84 = "+proj=longlat +datum=WGS84 +no_defs +type=crs"
 
+
+########
+# 1) Get OSM data
+######
+
+if(processing_steps['download_osm']){
+
+  dir.create(osm_data_dir,recursive = T, showWarnings = F)
+  
+  Sys.setenv(OSMEXT_DOWNLOAD_DIRECTORY = osm_data_dir)
+
+  options(timeout = 3600)
+  for(country in unique(unlist(cities))){
+    oe_get(country, max_file_size = 5e10)
+  }
+
+  # TODO proper metadata export
+  #write(rjson::toJSON(list(provider='geofabrik',date=as.character(Sys.Date()), path='./data/osm/')),'osm.json')
+
+}
+  
+
 # extract OSM networks
-for(city in names(cities)){
-  city_centroid = st_transform(st_centroid(st_sfc(st_multipoint(t(osmdata::getbb(city)))) %>% st_set_crs(wgs84)), etrs89lcc)
-  bbox_data = c(st_coordinates(city_centroid)[,1] - max_bbox_radius*1000, st_coordinates(city_centroid)[,2] - max_bbox_radius*1000, 
+
+if(processing_steps['road_network']){
+
+  for(city in names(cities)){
+    city_centroid = st_transform(st_centroid(st_sfc(st_multipoint(t(osmdata::getbb(city)))) %>% st_set_crs(wgs84)), etrs89lcc)
+    bbox_data = c(st_coordinates(city_centroid)[,1] - max_bbox_radius*1000, st_coordinates(city_centroid)[,2] - max_bbox_radius*1000, 
                 st_coordinates(city_centroid)[,1] - max_bbox_radius*1000, st_coordinates(city_centroid)[,2] + max_bbox_radius*1000,
                 st_coordinates(city_centroid)[,1] + max_bbox_radius*1000, st_coordinates(city_centroid)[,2] - max_bbox_radius*1000,
                 st_coordinates(city_centroid)[,1] + max_bbox_radius*1000, st_coordinates(city_centroid)[,2] + max_bbox_radius*1000
                 )
-  broad_bbox = st_bbox(st_transform(st_sfc(st_multipoint(matrix(data = bbox_data, ncol = 2, byrow = T)), crs = etrs89lcc), wgs84)) %>% st_as_sfc()
+    broad_bbox = st_bbox(st_transform(st_sfc(st_multipoint(matrix(data = bbox_data, ncol = 2, byrow = T)), crs = etrs89lcc), wgs84)) %>% st_as_sfc()
   
-  #network =  st_sf(st_sfc(),crs = wgs84)
-  #for(country in cities[city]){
-  #  datafile = paste0('geofabrik_',strsplit(tail(strsplit(oe_match(country)$url,'/',fixed=T)[[1]], n=1),'.',fixed=T)[[1]][1], '.gpkg')
-  #  allnw = st_read(paste0(osm_data_dir, datafile), 'lines') %>% dplyr::filter(!is.na(highway)) %>% st_transform(wgs84)
-  #  network = rbind(network,
-  #    #oe_get(place=country, query = "SELECT * FROM lines WHERE highway IS NOT NULL",
-  #    #       boundary = broad_bbox, boundary_type = "spat")
-  #    st_intersection(allnw,broad_bbox)
-  #  )
-  #}
+    dir.create(paste0(data_dir,city),showWarnings = F)
   
-  #if(!cached_city_data){dir}
-  dir.create(paste0(data_dir,city),showWarnings = F)
-  
-  for(country in cities[city]){
-    datafile = paste0('geofabrik_',strsplit(tail(strsplit(oe_match(country)$url,'/',fixed=T)[[1]], n=1),'.',fixed=T)[[1]][1])
+    for(country in cities[city]){
+      datafile = paste0('geofabrik_',strsplit(tail(strsplit(oe_match(country)$url,'/',fixed=T)[[1]], n=1),'.',fixed=T)[[1]][1])
     
-    # osmosis --read-pbf geofabrik_luxembourg-latest.osm.pbf --bounding-box top=49.61773 left=6.121288 bottom=49.59816 right=6.151396 --tf accept-ways highway=* --used-node --write-pbf test.osm.pbf
-    system(paste0('osmosis --read-pbf ', osm_data_dir, datafile, '.osm.pbf --bounding-box top=', st_bbox(broad_bbox)$ymax, ' left=',st_bbox(broad_bbox)$xmin, ' bottom=',st_bbox(broad_bbox)$ymin, ' right=',st_bbox(broad_bbox)$xmax,' --tf accept-ways highway=* --used-node --write-pbf ',osm_data_dir, datafile,'_',city,'_highways.osm.pbf'))
+      # osmosis --read-pbf geofabrik_luxembourg-latest.osm.pbf --bounding-box top=49.61773 left=6.121288 bottom=49.59816 right=6.151396 --tf accept-ways highway=* --used-node --write-pbf test.osm.pbf
+      system(paste0('osmosis --read-pbf ', osm_data_dir, datafile,
+                  '.osm.pbf --bounding-box top=', st_bbox(broad_bbox)$ymax,
+                  ' left=',st_bbox(broad_bbox)$xmin, ' bottom=',st_bbox(broad_bbox)$ymin,
+                  ' right=',st_bbox(broad_bbox)$xmax,' --tf accept-ways highway=* --used-node --write-pbf ',
+                  osm_data_dir, datafile,'_',city,'_highways.osm.pbf')
+           )
     
-    # not needed
-    # ogr2ogr test.gpkg test.osm.pbf 
-    #system(paste0('ogr2ogr ',osm_data_dir, datafile,'_',city,'_highways.gpkg ',osm_data_dir, datafile,'_',city,'_highways.osm.pbf'))
+      # not needed
+      # ogr2ogr test.gpkg test.osm.pbf 
+      #system(paste0('ogr2ogr ',osm_data_dir, datafile,'_',city,'_highways.gpkg ',osm_data_dir, datafile,'_',city,'_highways.osm.pbf'))
+      
+      system(paste0('cp ',osm_data_dir, datafile,'_',city,'_highways.osm.pbf ',data_dir,city,'/highways.osm.pbf'))
     
-    system(paste0('cp ',osm_data_dir, datafile,'_',city,'_highways.osm.pbf ',data_dir,city,'/highways.osm.pbf'))
-    
-  }
+    }
   
   
-  # TODO merge if more than one country
-  # cp data/osm/geofabrik_luxembourg-latest_Luxembourg_highways.osm.pbf 
+    # TODO merge if more than one country
+    # cp data/osm/geofabrik_luxembourg-latest_Luxembourg_highways.osm.pbf 
    
-  #st_write(network, dsn = paste0(data_dir,city,'/network.gpkg'), layer = city)
-  #sf::gdal_utils(util = "vectortranslate", source = paste0(data_dir,city,'/network.gpkg'), destination = paste0(data_dir,city,'/network.osm.pbf'),options = c("-f", "OSM"))
+    #st_write(network, dsn = paste0(data_dir,city,'/network.gpkg'), layer = city)
+    #sf::gdal_utils(util = "vectortranslate", source = paste0(data_dir,city,'/network.gpkg'), destination = paste0(data_dir,city,'/network.osm.pbf'),options = c("-f", "OSM"))
+  }
   
 }
 
@@ -219,6 +208,11 @@ points_dest = point_grid(st_centroid(broad_bbox), 1000, 10)
 dest = st_coordinates(points_dest)
 dest = data.frame(id = as.character(1:nrow(dest)), lon=dest[,1], lat=dest[,2])
 
+
+
+
+# 2) Construct multimodal isochrone
+
 # pete en mem avec 100km
 #isochrone = r5r::isochrone(network,origins = orig ,mode=c( "TRANSIT", "CAR"))
 #plot(isochrone[2,])
@@ -227,7 +221,7 @@ travel_time = travel_time_matrix(network, orig, dest,mode=c("TRANSIT","CAR"),
                                  max_trip_duration = 240,verbose = T, progress = T)
 dest$time = rep(NA, nrow(dest))
 dest$time[as.numeric(travel_time$to_id)] = travel_time$travel_time_p50
-ggplot(dest)+geom_point(aes(x=lon,y=lat,fill=time),size=2, shape=1)
+ggplot(dest[!is.na(dest$time),])+geom_point(aes(x=lon,y=lat,color=time),size=2, shape=1)
 
 det = detailed_itineraries(network, orig, dest,mode=c("TRANSIT","CAR"),
                            max_trip_duration = 240,verbose = T, progress = T)
@@ -241,9 +235,4 @@ ggplot() +geom_sf(data = street_net$edges, color='gray85') +
 # TODO heuristic to calculate approximate isochrones : PT+WALK+BICYCLE, CAR, PT+CAR
 
 
-# 2) Get buildings within the area
 
-
-
-
-# export data : zenodo, nakala? (flat), (~dataverse~)
